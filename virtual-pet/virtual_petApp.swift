8f5d27e45bbac5213cbcbc7c812fa0c6aa442428
s-hann <hanna22n@gmail.com>
2025-05-16 14:29:54 +0800

    Initial Commit

---
+++ virtual-pet/virtual_petApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtual_petApp.swift
+//  virtual-pet
+//
+//  Created by Hanna Nadia Savira on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct virtual_petApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
