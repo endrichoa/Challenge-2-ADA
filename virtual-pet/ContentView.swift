e88c0e616f8a40e245ea031168109599fb867d32
shreyasvenadan <s3944652@student.rmit.edu.au>
2025-05-21 14:05:01 +0800

    my commit

--- virtual-pet/ContentView.swift
+++ virtual-pet/ContentView.swift
@@ -14,6 +14,14 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Iko!")
+            Text("Hello, Iko!")
+            Text("Hello, Iko!")
+            Text("Hello, Iko!")
+            Text("Hello, Iko!")
+            Text("Hello, Iko!")
+            Text("Hello, Iko!")
+            Text("Hello, Iko!")
+            Text("Hello, Iko!")
         }
         .padding()
     }
